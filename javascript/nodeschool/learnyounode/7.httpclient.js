9013c2f15853c94856eddfeb6d7958c9c3098542
Marco Gablowsky <marco.gablowsky@viaprinto.de>
2014-09-29 12:19:51 +0200

    learnyounode ex 7

---
+++ javascript/nodeschool/learnyounode/7.httpclient.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    url = process.argv[2];
+
+http.get(url, function(response){
+    response.setEncoding('utf8');
+    response.on("data", function(data){
+        console.log(data);
+    });
+}).on('error', function(e){
+    throw e;
+});
