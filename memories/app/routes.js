e077fc50ec4ff66a496ededc5f6893de1a350d7f
marco <m.gablowsky@googlemail.com>
2014-12-02 22:09:38 +0100

    finished crud operations on game model

--- memories/app/routes.js
+++ memories/app/routes.js
@@ -2,13 +2,19 @@ var Game = require('./models/Game');
 
 module.exports = function(app) {
 
+    var mapRequestToGame = function(req, game){
+        game.name = req.body.name;
+        game.description = req.body.description;
+        game.cards = req.body.cards;
+        game.thumbnailLink = req.body.thumbnailLink;
+    };
+
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
     app.post('/games', function(req, res){
         var game = new Game();
-        game.name = req.body.name;
-        game.description = req.body.description;
+        mapRequestToGame(req,game);
         game.save(function(err){
             if(err){
                 res.send(err);
@@ -23,6 +29,37 @@ module.exports = function(app) {
                 res.send(err);
             }
             res.json(games);
+        });
+    });
+
+    app.get('/games/:game_id',function(req, res){
+        Game.findById(req.params.game_id, function(err, game){
+            if(err){
+                res.send(err);
+            }
+            res.json(game);
+        })
+    });
+    app.put('/games/:game_id',function(req, res){
+        Game.findById(req.params.game_id, function(err, game){
+            if(err){
+                res.send(err);
+            }
+            mapRequestToGame(req,game);
+            game.save(function(err){
+                if(err){
+                    res.send(err);
+                }
+                res.json({message: 'Game updated'});
+            });
+        });
+    });
+    app.delete('/games/:game_id',function(req, res){
+        Game.remove({_id: req.params.game_id}, function(err){
+            if(err){
+                res.send(err);
+            }
+            res.json({message: 'Deleted'});
         });
     });
 
