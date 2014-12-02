03b02ac0a96b40d6b274c7773d918893f95eb917
marco <m.gablowsky@googlemail.com>
2014-12-02 17:40:46 +0100

    simple node based rest api tutorial

---
+++ javascript/rest-api-tutorial/server.js
@@ -0,0 +1,83 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Bear = require('./app/models/bear');
+var port = 1337;
+
+var app = express();
+var router = express.Router();
+
+mongoose.connect('mongodb://localhost/memories_db');
+
+router.use(function(req, res, next){
+    console.log('Something is happening.');
+    next(); // make sure to go to next routes and do not stop here
+});
+
+router.get('/', function(req, res){
+    res.json({ message: 'Hooray! Welcome to our api!'});
+});
+
+router.route('/bears')
+    .post(function(req, res){
+        var bear = new Bear();
+        bear.name = req.body.name;
+
+        bear.save(function(err){
+            if(err){
+                res.send(err);
+            }
+            res.json({message: 'Bear created!'});
+        });
+    })
+    .get(function(req, res){
+        Bear.find(function(err, bears){
+            if(err){
+                res.send(err);
+            }
+            res.json(bears);
+        });
+    });
+
+router.route('/bears/:bear_id')
+    .get(function(req, res){
+        Bear.findById(req.params.bear_id, function(err, bear){
+            if(err){
+                res.send(err);
+            }
+            res.json(bear);
+        })
+    })
+    .put(function(req, res){
+        Bear.findById(req.params.bear_id, function(err, bear){
+            if(err){
+                res.send(err);
+            }
+
+            bear.name = req.body.name;
+
+            bear.save(function(err){
+                if(err){
+                    res.send(err);
+                }
+                res.json({message: 'bear updated'});
+            });
+        });
+    })
+    .delete(function(req, res){
+        Bear.remove({_id: req.params.bear_id}, function(err, bear){
+            if(err){
+                res.send(err);
+            }
+            res.json({message: 'Deleted'});
+        });
+    });
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use('/api',router);
+
+app.listen(port);
+console.log('Started on port '+port);
