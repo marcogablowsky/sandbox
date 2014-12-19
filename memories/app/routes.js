febc8a9de72c7cd35bf7590c51bde60b86bad313
Marco Gablowsky <marco.gablowsky@viaprinto.de>
2014-12-19 15:37:18 +0100

    separated route definitions

--- memories/app/routes.js
+++ memories/app/routes.js
@@ -1,67 +1,10 @@
-var Game = require('./models/Game');
 
 module.exports = function(app) {
 
-    var mapRequestToGame = function(req, game){
-        game.name = req.body.name;
-        game.description = req.body.description;
-        game.cards = req.body.cards;
-        game.thumbnailLink = req.body.thumbnailLink;
-    };
-
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
-    app.post('/games', function(req, res){
-        var game = new Game();
-        mapRequestToGame(req,game);
-        game.save(function(err){
-            if(err){
-                res.send(err);
-            }
-            res.json({message: 'Game created!'});
-        });
-    });
-
-    app.get('/games', function(req,res){
-        Game.find(function(err, games){
-            if(err){
-                res.send(err);
-            }
-            res.json(games);
-        });
-    });
-
-    app.get('/games/:game_id',function(req, res){
-        Game.findById(req.params.game_id, function(err, game){
-            if(err){
-                res.send(err);
-            }
-            res.json(game);
-        })
-    });
-    app.put('/games/:game_id',function(req, res){
-        Game.findById(req.params.game_id, function(err, game){
-            if(err){
-                res.send(err);
-            }
-            mapRequestToGame(req,game);
-            game.save(function(err){
-                if(err){
-                    res.send(err);
-                }
-                res.json({message: 'Game updated'});
-            });
-        });
-    });
-    app.delete('/games/:game_id',function(req, res){
-        Game.remove({_id: req.params.game_id}, function(err){
-            if(err){
-                res.send(err);
-            }
-            res.json({message: 'Deleted'});
-        });
-    });
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+    require('./routes/game-routes')(app);
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
---
+++ memories/app/routes/game-routes.js
@@ -0,0 +1,55 @@
+var Game = require('./models/Game');
+
+module.exports = function(app){
+    var mapRequestToGame = function(req, game){
+        game.name = req.body.name;
+        game.description = req.body.description;
+        game.cards = req.body.cards;
+        game.thumbnailLink = req.body.thumbnailLink;
+    };
+
+    var sendErrorResponseOnError = function(err,res){
+        if(err){
+            res.send(err);
+        }
+    };
+
+    app.post('/games', function(req, res){
+        var game = new Game();
+        mapRequestToGame(req,game);
+        game.save(function(err){
+            sendErrorResponseOnError(err,res);
+            res.json({message: 'Game created!'});
+        });
+    });
+
+    app.get('/games', function(req,res){
+        Game.find(function(err, games){
+            sendErrorResponseOnError(err,res);
+            res.json(games);
+        });
+    });
+
+    app.get('/games/:game_id',function(req, res){
+        Game.findById(req.params.game_id, function(err, game){
+            sendErrorResponseOnError(err,res);
+            res.json(game);
+        })
+    });
+    app.put('/games/:game_id',function(req, res){
+        Game.findById(req.params.game_id, function(err, game){
+            sendErrorResponseOnError(err,res);
+            mapRequestToGame(req,game);
+            game.save(function(err){
+                sendErrorResponseOnError(err,res);
+                res.json({message: 'Game updated'});
+            });
+        });
+    });
+    app.delete('/games/:game_id',function(req, res){
+        Game.remove({_id: req.params.game_id}, function(err){
+            sendErrorResponseOnError(err,res);
+            res.json({message: 'Deleted'});
+        });
+    });
+};
